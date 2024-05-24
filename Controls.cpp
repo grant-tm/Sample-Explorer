df88f31b582078f064503773cfaf72b92b17a058
grant-tm <grant.t.morgan@gmail.com>
2024-05-23 21:34:47 -0700

    add input hold detection to keyboard driver

--- Controls.cpp
+++ Controls.cpp
@@ -1,52 +1,91 @@
 #include "Controls.h"
 
-inline bool is_pressed (char c) {
-    return GetAsyncKeyState(c) & 0x8000;
+bool held_keys[256] = { false };
+
+inline bool key_is_pressed (int win_key) {
+    if(win_key > 0 && win_key < 255) {
+        bool pressed = (GetAsyncKeyState(win_key) & 0x8000) != 0;
+        
+        if (pressed) {
+            held_keys[win_key] = true;
+        } else if(held_keys[win_key]) {
+            held_keys[win_key] = false;
+        }
+
+        return pressed;
+    }
+    return false;
+}
+
+inline bool key_is_held (int win_key) {
+    return held_keys[win_key];
+}
+
+inline bool key_is_newly_pressed (int win_key) {
+    bool held = key_is_held(win_key);
+    bool pressed = key_is_pressed(win_key);
+    return pressed && !held;
+}
+
+inline bool key_is_whitelisted (int win_key) {
+    return win_key != 16 && win_key != 17;
 }
 
 char detect_commands (void) {
     // Check if a specific key is pressed
-    if(is_pressed(VK_CONTROL)) {
-        if (is_pressed('S')) {
-            return 19;
+    if(key_is_pressed(VK_CONTROL)) {
+        if (key_is_newly_pressed('S')) {
+            return KBD_CTRL_S;
         }
-        else if (is_pressed('R')) {
-            return 18;
+        else if (key_is_newly_pressed('R')) {
+            return KBD_CTRL_R;
         }
-        else if (is_pressed('T')) {
-            return 20;
+        else if (key_is_newly_pressed('T')) {
+            return KBD_CTRL_T;
         }
-        else if (is_pressed('Q')) {
-            return 17;
+        else if (key_is_newly_pressed('Q')) {
+            return KBD_CTRL_Q;
         }
     }
     return 0;
 }
 
 char vk_to_char (int key) {
-    if(key == 0x0D) {
-        return '\n';
+    char ret = '\0';
+    switch(key) {
+        case KBD_RETURN:
+            ret = '\n';
+            break;
+        case KBD_BACKSPACE:
+            ret = '\b';
+            break;
+        case KBD_DELETE:
+            ret = 127;
+            break;
+        case KBD_SHIFT:
+            ret = '\0';
+            break;
+        default:
+            ret = key;
     }
-    if(key == 0x08) {
-        return '\b';
-    }
-    if(key == 0x2E) {
-        return 127;
-    }
-    return key;
+    return ret;
 }
 
+// Return first command or valid keyboard press
 char keyboard_listener (void) {
     while (1) {
+        // Handle commands
         char comm = detect_commands();
         if(comm != 0) {
             return comm;
         }
-        for (int key = 8; key <= 255; ++key) {
-            if (is_pressed(key) && key != 17) {
+        
+        // Handle regular keys
+        for (int key = 8; key < 256; ++key) {
+            if (key_is_newly_pressed(key) && key_is_whitelisted(key)) {
                 return vk_to_char(key);
             }
         }
-        Sleep(10);
+        Sleep(20);
     }
 }
--- Controls.h
+++ Controls.h
@@ -2,13 +2,25 @@
 #define CONTROLS_H
 
 #include <windows.h>
+#include "SystemUtilities.h"
 
-inline bool is_pressed (char c);
+#define KBD_CTRL_S 19
+#define KBD_CTRL_R 18
+#define KBD_CTRL_T 20
+#define KBD_CTRL_Q 17
 
-char detect_commands (void);
+#define KBD_RETURN 10
+#define KBD_BACKSPACE 8
+#define KBD_DELETE 127
+#define KBD_SHIFT 16
+
+inline bool is_held (int win_key);
+inline bool is_pressed (int win_key);
+inline bool is_whitelisted(int win_key);
 
 char vk_to_char (int key);
 
+char detect_commands (void);
 char keyboard_listener (void);
 
 #endif // CONTROLS_H
