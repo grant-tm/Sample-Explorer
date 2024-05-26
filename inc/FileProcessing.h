8f95bb127c291deb1e5735c66e74c8ad5beed01d
grant-tm <grant.t.morgan@gmail.com>
2024-05-25 23:59:12 -0700

    update file processing header

--- inc/FileProcessing.h
+++ inc/FileProcessing.h
@@ -5,10 +5,9 @@
 #include <string>
 #include <vector>
 #include <filesystem>
+#include <thread>
 
 // External Inclusions
-#include <omp.h>
-#include <thread>
 #include "sqlite3.h"
 
 // Project Inclusions
@@ -22,43 +21,46 @@ namespace fs = std::filesystem;
 #define TRANSACTION_SIZE 2048
 
 // Delimiter check function
-constexpr inline bool char_is_delimiter(char);
+constexpr inline bool char_is_delimiter (char);
 
 // Character to lowercase conversion
-constexpr inline char to_lower(char);
+constexpr inline char to_lower (char);
 
 // Tag generation function
-std::vector<std::string> generate_auto_tags(const std::string &);
+std::vector<std::string> generate_auto_tags (const std::string &);
 
 // Tag concatenation function
-std::string concatenate_tags(const std::vector<std::string> &);
+std::string concatenate_tags (const std::vector<std::string> &);
 
 // File processing function
-struct ExplorerFile *process_file(sqlite3 *, const fs::directory_entry &);
+struct ExplorerFile *process_file (sqlite3 *, const fs::directory_entry &);
 
 // File extension validation
-inline bool validate_file_extension(const fs::directory_entry *);
+inline bool validate_file_extension (const fs::directory_entry *);
 
 // Sub-directory finding function
-std::vector<fs::path> find_sub_dirs(const fs::path &);
+std::vector<fs::path> find_sub_dirs (const fs::path &);
 
 // File processing requirement check
-bool requires_processing(sqlite3 *, const fs::directory_entry *);
+bool requires_processing (sqlite3 *, const fs::directory_entry *);
 
-// File queueing function
-void queue_files_for_processing(sqlite3 *, const fs::path &, 
+// File queueing functions
+void queue_files (sqlite3 *, const fs::path &, 
+                ThreadSafeQueue<fs::directory_entry> *);
+
+void queue_all_files (sqlite3 *, const fs::path &, 
                 ThreadSafeQueue<fs::directory_entry> *);
 
 // Processing queued files function
-void process_queued_files(sqlite3 *, 
+void process_queued_files (sqlite3 *, 
         ThreadSafeQueue<fs::directory_entry> *,
         ThreadSafeQueue<struct ExplorerFile *> *);
 
 // Insert processed files function
-void insert_processed_files(sqlite3*, 
+void insert_processed_files (sqlite3*, 
     ThreadSafeQueue<struct ExplorerFile *> *);
 
 // Directory scanning function
-void scan_directory(sqlite3 *, const fs::path &);
+void scan_directory (sqlite3 *, const fs::path &);
 
 #endif // FILE_PROCESSING_H
